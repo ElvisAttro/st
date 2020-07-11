bcfa22ad9e8b95c9296f4d640d0d89bd7ab82070
Elvis Attro <attroelvis@gmail.com>
2020-07-11 21:20:01 +0000

    Apply patches/keyboard_select/st-keyboard_select-20200617-9ba7ecf.diff

--- config.def.h
+++ config.def.h
@@ -211,6 +211,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
+	{ TERMMOD,              XK_Escape,      keyboard_select,{.i =  0} },
 	{ WINMOD,               XK_Return,      newterm,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
